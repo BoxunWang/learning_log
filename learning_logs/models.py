f7ea5286d246ae6db8a095583831052612e6e170
BoxunWang <boxunwangzbc@gmail.com>
2018-12-28 15:10:42 +0800

    Update models.py -- Entry
    更新models.py -- 添加Entry
    迁移模型Entry
    更新笔记18.2.3-5

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -11,3 +11,14 @@ class Topic(models.Model):
 		"""返回模型的字符串表示"""
 		return self.text
 
+class Entry(models.Model):
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		"""返回模型8的字符串表示"""
+		return self.text[:50] + "..."
