760e1dbbc0b170da377c5477f575c50aa86c0f2c
BoxunWang <boxunwangzbc@gmail.com>
2018-12-29 11:07:27 +0800

    Update urls.py, views.py. Create index.html
    编写视图views.py，编写模板index.html
    更笔记18.3.2-3

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls import url
 from . import views
 #包含项目中的应用程序的URL
 urlpatterns = [
-	"""
+	#index page
+	url(r'^$', views.index, name='index'),
+]
+"""
 	URL接收三个实参：
 	1.正则表达式
 		r 	将接下来的字符串视为原始字符串 
@@ -14,7 +17,4 @@ urlpatterns = [
 		$ 	查看字符串的末尾
 	2.指定调用的视图函数
 	3.指定该URL模式的名称
-	"""
-	#index page
-	url(r'^$', views.index, name='index'),
-]
+"""
